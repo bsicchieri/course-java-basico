c986cb521cd168ca35478bc7ea790675777e429b
bsicchieri <brunosicchieri@hotmail.com>
2021-05-18 17:12:35 -0300

    aula10

--- src/com/bsicchieri/cursojava/aula10/Variaveis.java
+++ src/com/bsicchieri/cursojava/aula10/Variaveis.java
@@ -1,5 +1,14 @@
 package com.bsicchieri.cursojava.aula10;
 
 public class Variaveis {
-    
+
+    public static void main(String[] args) {
+
+        int idade = 20;
+        String nome;
+
+        System.out.println("Idade = " + idade);
+
+    }
+
 }
