f9338758c9d55f90fc170dfc667bbafc80017073
bsicchieri <brunosicchieri@hotmail.com>
2021-05-20 00:16:30 -0300

    aula24

---
+++ src/com/bsicchieri/cursojava/aula24/Carro.java
@@ -0,0 +1,15 @@
+package com.bsicchieri.cursojava.aula24;
+
+import java.util.Date;
+
+public class Carro {
+
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+    String[] vendedores;
+    Date dataLancamento;
+
+}
